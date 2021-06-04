51d6ce94fc10e9e738baee2dd5211e204f088053
tasya-cnp <77194777+tasya-cnp@users.noreply.github.com>
2021-06-04 20:38:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,57 @@
+img = "";
+status = ""; 
+objects = [];
+
+function preload()
+{
+    img = loadImage('dog_cat.jpg');
+    
+}
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Object";
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(img, gotResult());
+}
+
+function gotResult(error, results)
+{
+   if (error) {
+       console.error(error);
+   } else 
+   {
+       console.log(results);
+       objects = results;
+       
+   }
+}
+
+function draw()
+{
+    image(img, 0, 0, 640, 420);
+
+    if(status != "")
+    {
+      for (i = 0; i < objects.lenght; i++)
+      {
+          document.getElementById("status").innerHTML = "Status : Object Dected";
+          fill ("#FF0000");
+          percent = floor(objects[i].confidence * 100);
+          text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+          noFill();
+          stroke("#FF0000");
+          rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+      }   
+    }
+   
+}
